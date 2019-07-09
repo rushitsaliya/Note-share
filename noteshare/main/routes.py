a8db26a91259d80d373cb9bde7a573dea3c0c51c
RushitSaliya <rushitsaliya99@gmail.com>
2019-07-09 09:22:13 +0530

    Make sidebar working

--- noteshare/main/routes.py
+++ noteshare/main/routes.py
@@ -4,8 +4,17 @@ from noteshare.models import Note
 main = Blueprint('main', __name__)
 
 @main.route('/')
-@main.route('/home')
+@main.route('/home', methods=['GET'])
 def home():
     page = request.args.get('page', 1, type=int)
-    notes = Note.query.order_by(Note.date_posted.desc()).paginate(page=page, per_page=5)
+    filter = request.args.get('filter', None)
+
+    if filter is None:
+        notes = Note.query.order_by(Note.date_posted.desc()).paginate(page=page, per_page=5)
+    elif filter == "latest":
+        notes = Note.query.order_by(Note.date_posted.desc()).paginate(page=page, per_page=5)
+    elif filter == "earlier":
+        notes = Note.query.order_by(Note.date_posted.asc()).paginate(page=page, per_page=5)
+    else:
+        pass
     return render_template('home.html', title='Home', notes=notes)
