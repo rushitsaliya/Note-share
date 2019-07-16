07baff0ecdc63921f0c4e0bb354e62ce98f48058
RushitSaliya <rushitsaliya99@gmail.com>
2019-07-16 17:41:39 +0530

    Make note Like and Dislike feature work

--- noteshare/notes/routes.py
+++ noteshare/notes/routes.py
@@ -6,6 +6,7 @@ from noteshare.notes.forms import NoteForm
 
 notes = Blueprint('notes', __name__)
 
+
 @notes.route('/note/new', methods=['GET', 'POST'])
 @login_required
 def new_note():
@@ -60,3 +61,23 @@ def user_notes(username):
     user = User.query.filter_by(username=username).first_or_404()
     notes = Note.query.filter_by(author=user).order_by(Note.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('user_notes.html', title=user.username, notes=notes, user=user)
+
+
+@notes.route('/note/<int:note_id>/up', methods=['GET', 'POST'])
+@login_required
+def up_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    note.ups += 1
+    db.session.commit()
+    from_page = request.args.get('from_page')
+    return redirect(from_page) if from_page else redirect(url_for('main.home'))
+
+
+@notes.route('/note/<int:note_id>/down', methods=['GET', 'POST'])
+@login_required
+def down_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    note.downs += 1
+    db.session.commit()
+    from_page = request.args.get('from_page')
+    return redirect(from_page) if from_page else redirect(url_for('main.home'))
